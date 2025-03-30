148facdd51f613b616782be1fe0690bc3d605658
bestJaneever <eugeniagrebenko@gmail.com>
2025-03-30 08:40:50 +0200

    Add pymssql

---
+++ dbService_pymssql.py
@@ -0,0 +1,26 @@
+import pymssql
+import pandas as pd
+
+SERVER = "127.0.0.1"
+DATABASE = "TRN"
+USERNAME = "dbo_user"
+PASSWORD = "@11dbo_user_for_RF"
+
+
+class dbServicePymssql:
+    def create_connection(self) -> pymssql.Connection:
+        connection = pymssql.connect(
+            server=SERVER,
+            user=USERNAME,
+            password=PASSWORD,
+            database=DATABASE,
+            as_dict=True
+        )
+        return connection
+
+    def execute_query(self, query) -> pd.DataFrame:
+        connection = self.create_connection()
+        cursor = connection.cursor()
+        cursor.execute(query)
+        data = cursor.fetchall()
+        return pd.DataFrame(data)
